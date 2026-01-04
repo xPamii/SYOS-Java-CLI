a17d00d50da0622924309820ff10e5a628baeb9e
xPamii <pbherath2018@gmail.com>
2026-01-05 02:50:56 +0530

    Update Batch model

--- SYOS/src/main/java/com/syos/model/Batch.java
+++ SYOS/src/main/java/com/syos/model/Batch.java
@@ -1,4 +1,41 @@
 package com.syos.model;
 
+import java.time.LocalDate;
+
 public class Batch {
+
+    LocalDate purchaseDate;
+    LocalDate expiryDate;
+    int quantity;
+
+    public Batch(LocalDate expiryDate, int quantity, LocalDate purchaseDate) {
+        this.expiryDate = expiryDate;
+        this.quantity = quantity;
+        this.purchaseDate = purchaseDate;
+    }
+
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(LocalDate purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
 }
